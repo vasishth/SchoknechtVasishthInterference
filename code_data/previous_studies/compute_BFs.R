ecf65e31af2b47b400f4c38255d4d6f4ed1954d2
Shravan Vasishth <vasishth.shravan@gmail.com>
2024-08-26 14:39:54 +0200

    Updated via the Open Science Framework

--- code_data/previous_studies/compute_BFs.R
+++ code_data/previous_studies/compute_BFs.R
@@ -1,13 +1,13 @@
 # author: Pia Schoknecht
 # date: 09.07.2024
 
-# script to compute Bayes Factors with the Savage-Dickey method
+# script to compute Bayes Factors with the Savage-Dickey method for Mertzen et al. (2023)
 library(tidyverse)
 library(brms)
 library(ggplot2)
 library(bayesplot)
 
-
+# load English model fits
 load("model_fits_E/Fit_s_Full_precrit.Rda")
 load("model_fits_E/Fit_m_Full_precrit.Rda")
 load("model_fits_E/Fit_l_Full_precrit.Rda")
@@ -19,6 +19,7 @@ load("model_fits_E/Fit_l_Full_crit.Rda")
 load("model_fits_E/Fit_s_Full_postcrit.Rda")
 load("model_fits_E/Fit_m_Full_postcrit.Rda")
 load("model_fits_E/Fit_l_Full_postcrit.Rda")
+
 
 #bfs 
 
@@ -36,44 +37,94 @@ compute_bfs <- function(model){
 }
 
 # save BFs in df
-df.bf_m23.E <- data.frame(matrix(ncol=8,nrow=0))
-colnames(df.bf_m23.E) <- c("Experiment", "BF_method", "Language", "Region", "Effect","Prior","truncated", "BF10")
+df.bf_m23 <- data.frame(matrix(ncol=8,nrow=0))
+colnames(df.bf_m23) <- c("Experiment", "BF_method", "Language", "Region", "Effect","Prior","truncated", "BF10")
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "pre-critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[3],2))
 
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[1],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[2],2))
-df.bf_m23.E[nrow(df.bf_m23.E)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[3],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "English", "spill-over", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[3],2))
 
 
-save(df.bf_schoknecht, file = paste("BFs_schoknecht_precrit.Rda"))
+# load German model fits
+load("model_fits_G/Fit_s_Full_precrit.Rda")
+load("model_fits_G/Fit_m_Full_precrit.Rda")
+load("model_fits_G/Fit_l_Full_precrit.Rda")
+
+load("model_fits_G/Fit_s_Full_crit.Rda")
+load("model_fits_G/Fit_m_Full_crit.Rda")
+load("model_fits_G/Fit_l_Full_crit.Rda")
+
+load("model_fits_G/Fit_s_Full_postcrit.Rda")
+load("model_fits_G/Fit_m_Full_postcrit.Rda")
+load("model_fits_G/Fit_l_Full_postcrit.Rda")
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_precrit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_precrit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "pre-critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_precrit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_crit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_crit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "critical", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_crit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "syntactic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "semantic","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "interaction","Normal(0, 0.01)","no", round(compute_bfs(m_s_full_postcrit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "syntactic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "semantic","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "interaction","Normal(0, 0.05)","no", round(compute_bfs(m_m_full_postcrit)[3],2))
+
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "syntactic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[1],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "semantic","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[2],2))
+df.bf_m23[nrow(df.bf_m23)+1,] <- c("M23", "S-D", "German", "spill-over", "interaction","Normal(0, 0.1)","no", round(compute_bfs(m_l_full_postcrit)[3],2))
+
+
+save(df.bf_m23, file = paste("BFs_mertzen23.Rda"))
