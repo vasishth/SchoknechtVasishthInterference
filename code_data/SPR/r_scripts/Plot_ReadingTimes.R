ae5feda153e53079dfb7e6145086a9e39bdf4435
Shravan Vasishth <vasishth.shravan@gmail.com>
2024-11-06 11:28:11 +0100

    Updated via the Open Science Framework

--- code_data/SPR/r_scripts/Plot_ReadingTimes.R
+++ code_data/SPR/r_scripts/Plot_ReadingTimes.R
@@ -1,5 +1,5 @@
 #author: "Pia Schoknecht"
-#date: "16 06 2023"
+#date: "05 11 2024"
 
 # load packages
 library(plyr)
@@ -7,6 +7,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(cowplot)
+library(ggh4x)
 
 # load data
 df1 <- read.csv("../data/pandora_web_data_allsessions_short.csv", sep=";")
@@ -105,14 +106,14 @@ p_hisyn <- ggplot(data=hisyn_summ_by_participants,aes(x=wordno,
   scale_y_continuous(name="observed reading\ntime in (ms)",breaks=log(seq(320,475,20)),labels= seq(320,475,20))+ 
   scale_x_continuous(breaks = c(0:21), labels= hisyn_words, expand = c(0.01, 0.01)) +
   xlab("")+
-  geom_text(aes(x = 12, y = 6.0, label = "distractor"), color="black") +
-  geom_text(aes(x = 5.1, y = 5.81, label = "subject"), color="black") +
-  geom_segment(aes(x = 5.05, y = 5.84, xend = 5.05, yend = 5.9), color="black",
+  geom_text(aes(x = 12, y = 6.01, label = "distractor"), color="black") +
+  geom_text(aes(x = 5.1, y = 5.83, label = "subject"), color="black") +
+  geom_segment(aes(x = 5.05, y = 5.88, xend = 5.05, yend = 5.99), color="black",
                 arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
   geom_segment(aes(x = 12, y = 5.98, xend = 12, yend = 5.91), color="black",
                 arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
-  geom_text(aes(x = 17, y =6.05, label = "critical verb"), color="black") +
-  geom_segment(aes(x = 17, y = 6.03, xend = 17, yend = 5.97), color="black",
+  geom_text(aes(x = 17, y =6.09, label = "critical verb"), color="black") +
+  geom_segment(aes(x = 17, y = 6.06, xend = 17, yend = 5.99), color="black",
                 arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
   theme(legend.position="none")
 
@@ -131,55 +132,21 @@ p_losyn <- ggplot(data=losyn_summ_by_participants,aes(x=wordno,
   ggtitle("Low syntactic interference")+
   scale_y_continuous(name="observed reading\ntime in (ms)",breaks=log(seq(320,475,20)),labels= seq(320,475,20))+ 
   scale_x_continuous(breaks = c(0:20), labels= losyn_words, expand = c(0.01, 0.01)) +
-  geom_text(aes(x = 11, y = 6.1, label = "distractor"), color="black") +
-  geom_text(aes(x = 5.1, y = 5.8, label = "subject"), color="black") +
-  geom_segment(aes(x = 5.05, y = 5.84, xend = 5.05, yend = 5.9), color="black",
+  geom_text(aes(x = 5.1, y = 5.83, label = "subject"), color="black") +
+  geom_segment(aes(x = 5.05, y = 5.88, xend = 5.05, yend = 5.99), color="black",
                arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
-  geom_segment(aes(x = 11, y = 6.08, xend = 11, yend = 6.0), color="black",
-               arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+ 
+  geom_text(aes(x = 11, y = 6.1, label = "distractor"), color="black") +
+  geom_segment(aes(x = 11, y = 6.077, xend = 11, yend = 6.01), color="black",
+               arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
   geom_text(aes(x = 16, y =6.08, label = "critical verb"), color="black") +
-  geom_segment(aes(x = 16, y = 6.06, xend = 16, yend = 5.98), color="black",
+  geom_segment(aes(x = 16, y = 6.05, xend = 16, yend = 5.98), color="black",
                arrow = arrow(length = unit(0.03, "npc")), inherit.aes = FALSE)+
   scale_shape(guide="none")+
   theme(legend.position=c(0.435,0.73))  +
   theme(legend.key.size = unit(0.25, "cm"))+
   scale_linetype_discrete(name="Semantic Interference")
 
-#### Plot critical region (and surrounding regions) of all data ####
-lo <- filter(losyn_summ_by_participants,wordno %in% c(14:17))
-hi <- filter(hisyn_summ_by_participants,wordno %in% c(15:18))
-
-zoom <- rbind(lo,hi)
-
-zoom$region <- factor(zoom$region, levels = c("pre-pre-critical", "pre-critical", "critical", "spill-over"))
-zoom$word <- ifelse(zoom$region %in% c("pre-pre-critical"), "n-2",
-                    ifelse(zoom$region %in% c("pre-critical"), "n-1",
-                           ifelse(zoom$region %in% c("spill-over"), "n+1",
-                                  "critical")))
-
-zoom$word <- factor(zoom$word, levels = c("n-2", "n-1", "critical", "n+1"))
-
-zoom$highlight <- ifelse(zoom$word %in% c("critical"), "yes", "no")
-
-p_zoom <- ggplot(data=zoom, aes(x=word, y=meanlogrt,
-                                shape=syntactic.interference,
-                                linetype=semantic.interference,
-                                ymin=low2SE,ymax=high2SE)) +
-  geom_point(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), size=3, position= position_dodge(0.4)) + 
-  geom_line(aes(group = interaction(syntactic.interference, semantic.interference)), position= position_dodge(0.4), size=0.4) +
-  geom_errorbar(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), position= position_dodge(0.4), width = 0.2)+
-  scale_color_manual(values = c("black", "red"))+
-  theme_bw(base_size = 10)+
-  theme(legend.position="none")+
-  #theme(legend.position=c(0.8,0.3), legend.direction = "vertical")  +
-  scale_linetype_discrete(name="Semantic Interference")+
-  scale_shape_discrete(name="Syntactic Interference")+
-  ggtitle("E1a and E1b combined")+
-  theme(legend.text = element_text(size = 12))+
-  scale_y_continuous(name="observed reading time in (ms)", breaks=log(seq(340,390, 10)),labels= seq(340,390, 10))+
-  guides(color = "none")
-
-#### Plot critical region (and surrounding regions) per experiment ####
+#### Plot reading times of the whole sentence per experiment ####
 # start with by-experiment averages
 #calculate mean reading time per subject per word per condition
 hisyn_by_participants <- summarize(
@@ -207,90 +174,68 @@ losyn_summ_by_participants <- summarize(
   SE = sd(logrt)/sqrt(nrow(losyn_by_participants)), 
   low2SE= meanlogrt - 2*SE, high2SE= meanlogrt + 2*SE)
 
-
-
+# add nice condition labels
 hisyn_summ_by_participants$syntactic.interference <- ifelse(hisyn_summ_by_participants$condition %in% c("a","b"), "low", "high")
 hisyn_summ_by_participants$semantic.interference <- ifelse(hisyn_summ_by_participants$condition %in% c("a","c"), "low", "high")
 losyn_summ_by_participants$syntactic.interference <- ifelse(losyn_summ_by_participants$condition %in% c("a","b"), "low", "high")
 losyn_summ_by_participants$semantic.interference <- ifelse(losyn_summ_by_participants$condition %in% c("a","c"), "low", "high")
 
-losyn_summ_by_participants$region<- ifelse(losyn_summ_by_participants$wordno == 16, "critical",
-                                           ifelse(losyn_summ_by_participants$wordno == 15, "pre-critical", 
-                                                  ifelse(losyn_summ_by_participants$wordno == 14, "pre-pre-critical",        
-                                                         ifelse(losyn_summ_by_participants$wordno == 17, "spill-over",        
-                                                                "other"))))
 
-hisyn_summ_by_participants$region<- ifelse(hisyn_summ_by_participants$wordno == 17, "critical",
-                                           ifelse(hisyn_summ_by_participants$wordno == 16, "pre-critical", 
-                                                  ifelse(hisyn_summ_by_participants$wordno == 15, "pre-pre-critical",        
-                                                         ifelse(hisyn_summ_by_participants$wordno == 18, "spill-over",        
-                                                                "other"))))
-lo <- filter(losyn_summ_by_participants,wordno %in% c(14:17))
-hi <- filter(hisyn_summ_by_participants,wordno %in% c(15:18))
+# split exp info
+hisyn_summ_by_participants2 <- hisyn_summ_by_participants %>% separate(exp, c("exp", "more_info"), sep = ",", extra="merge")
+losyn_summ_by_participants2 <- losyn_summ_by_participants %>% separate(exp, c("exp", "more_info"), sep = ",", extra="merge")
 
-zoom <- rbind(lo,hi)
 
-zoom$word <- ifelse(zoom$region %in% c("pre-pre-critical"), "n-2",
-                    ifelse(zoom$region %in% c("pre-critical"), "n-1",
-                           ifelse(zoom$region %in% c("spill-over"), "n+1",
-                                  zoom$region)))
+# create minimal x-axis labels
+hisyn_words_minimal <- c("subject","distractor",  "critical\nverb")
+losyn_words_minimal <- c("subject","distractor","critical\nverb")
 
-zoom$word <- factor(zoom$word, levels = c("n-2", "n-1", "critical", "n+1"))
-
-zoom$highlight <- ifelse(zoom$word %in% c("critical"), "yes", "no")
-
-# split into sub-exps
-e1a_zoom <- zoom %>% filter(exp %in% c("E1a, items 1-60, N=204","E1a, items 61-120, N=160"))
-e1b_zoom <- zoom %>% filter(exp %in% c("E1b, items 1-60, N=350","E1b, items 61-120, N=220"))
-
-e1a_zoom2 <- e1a_zoom %>% separate(exp, c("exp", "more_info"), sep = ",", extra="merge")
-e1b_zoom2 <- e1b_zoom %>% separate(exp, c("exp", "more_info"), sep = ",", extra="merge")
-
-# plot
-p_e1a_zoom  <- ggplot(data=e1a_zoom2, aes(x=word, y=meanlogrt,
-                                          shape=syntactic.interference,
-                                          linetype=semantic.interference,
-                                          ymin=low2SE,ymax=high2SE)) +
-  geom_point(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), size=3, position= position_dodge(0.6)) + 
-  geom_line(aes(group = interaction(syntactic.interference, semantic.interference)), position= position_dodge(0.6), size=0.4) +
-  geom_errorbar(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), position= position_dodge(0.6), width = 0.2)+
-  scale_color_manual(values = c("black", "red"))+
+# hisyn plot per exp
+p_hisyn_exp <- ggplot(data=filter(hisyn_summ_by_participants2, wordno>4),aes(x=wordno, 
+                                                      y=meanlogrt,
+                                                      ymin=low2SE, 
+                                                      ymax=high2SE)) +
+  geom_point(aes(), size=1, position= position_dodge(0.1)) + 
+  geom_line(aes(linetype=semantic.interference)) +
+  geom_errorbar(aes(), position= position_dodge(0.1),size=.3, width = 0.2)+
+  theme(axis.text.x = element_text(size=7)) +
   theme_bw(base_size = 10)+
-  facet_grid(.~more_info)+
-  theme(legend.position=c(0.75,0.7), legend.direction = "vertical", legend.key.size = unit(0.25, "cm"))  +
-  ggtitle("E1a")+
-  scale_linetype_discrete(name="Semantic Interference")+
-  scale_shape_discrete(name="Syntactic Interference")+
-  theme(legend.text = element_text(size = 9))+
-  scale_y_continuous(name="observed reading time in (ms)", breaks=log(seq(300,410, 10)),labels= seq(300,410, 10))+
-  guides(color = "none")
-
-
-p_e1b_zoom <- ggplot(data=e1b_zoom2, aes(x=word, y=meanlogrt,
-                                         shape=syntactic.interference,
-                                         linetype=semantic.interference,
-                                         ymin=low2SE,ymax=high2SE)) +
-  geom_point(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), size=3, position= position_dodge(0.6)) + 
-  geom_line(aes(group = interaction(syntactic.interference, semantic.interference)), position= position_dodge(0.6), size=0.4) +
-  geom_errorbar(aes(group = interaction(syntactic.interference, semantic.interference),color=highlight), position= position_dodge(0.6), width = 0.2)+
-  scale_color_manual(values = c("black", "red"))+
-  theme_bw(base_size = 10)+
-  facet_grid(.~more_info)+
-  ggtitle("E1b")+
+  facet_nested(. ~ exp + more_info)+
+  ggtitle("High syntactic interference in each sub-experiment")+
+  scale_y_continuous(name="observed reading time in (ms)",breaks=log(seq(320,480,40)),labels= seq(320,480,40))+ 
+  scale_x_continuous(breaks = c(5, 12, 17), labels= hisyn_words_minimal, expand = c(0.01, 0.01)) +
+  xlab("")+
   theme(legend.position="none")+
-  scale_linetype_discrete(name="Semantic Interference")+
-  scale_shape_discrete(name="Syntactic Interference")+
-  theme(legend.text = element_text(size = 10))+
-  scale_y_continuous(name="observed reading time in (ms)", breaks=log(seq(300,410, 10)),labels= seq(300,410, 10))+
-  guides(color = "none")
+  theme(panel.spacing = unit(1.025, "lines"))
+
+p_hisyn_exp 
+
+# losyn plot per exp
+p_losyn_exp <- ggplot(data=filter(losyn_summ_by_participants2, wordno>4),aes(x=wordno, 
+                                                          y=meanlogrt,
+                                                          ymin=low2SE, 
+                                                          ymax=high2SE)) +
+  geom_point(aes(), shape=17, size=1, position= position_dodge(0.1)) + 
+  geom_line(aes(linetype=semantic.interference)) +
+  geom_errorbar(aes(), position= position_dodge(0.1),size=.3, width = 0.2)+
+  theme(axis.text.x = element_text(size=7)) +
+  theme_bw(base_size = 10)+
+  facet_nested(. ~ exp + more_info)+
+  ggtitle("Low syntactic interference in each sub-experiment")+
+  scale_y_continuous(name="observed reading time in (ms)",breaks=log(seq(320,480,40)),labels= seq(320,480,40))+ 
+  scale_x_continuous(breaks = c(5,11,16), labels= losyn_words_minimal, expand = c(0.01, 0.01)) +
+  xlab("")+
+  theme(legend.position="none")+
+  theme(panel.spacing = unit(1.025, "lines"))
+
+p_losyn_exp 
 
 # Put everything together
 p_sentence <- plot_grid(p_hisyn, p_losyn, nrow=2, labels=c("A", "B"))
-ggsave("Pandora_all_wholesentence_pooled.jpg", width=10.5, height=4.5, dpi=600)
+ggsave("Pandora_all_wholesentence_pooled.jpg", width=12, height=4.5, dpi=600)
 
-p_zooms <- plot_grid(p_zoom, p_e1a_zoom, p_e1b_zoom, ncol=3, rel_widths = c(1,1.45,1.45), labels=c("C", "D", "E"))
+p_exp <- plot_grid(p_hisyn_exp, p_losyn_exp, ncol=2, rel_widths = c(1.01,1), labels=c("C", "D"))
 
-
-plot_grid(p_sentence, p_zooms, ncol=1, rel_heights = c(1.2,1))
-ggsave("Pandora_all_wholesentence_pooled_zoom_exp.jpg", width=12, height=7, dpi=600)
+plot_grid(p_sentence, p_exp, ncol=1, rel_heights = c(1.5,1))
+ggsave("Pandora_all_wholesentence_pooled_exp.jpg", width=12, height=7, dpi=600)
 
